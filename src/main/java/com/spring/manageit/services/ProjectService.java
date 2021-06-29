536befa1c71753a2c96dd0e0e53cbe9a48ba1a3d
Nitish Kumar <nitishkmr096@gmail.com>
2021-06-30 01:03:30 +0530

    Delete an existing project

--- src/main/java/com/spring/manageit/services/ProjectService.java
+++ src/main/java/com/spring/manageit/services/ProjectService.java
@@ -29,4 +29,16 @@ public class ProjectService {
 		}
 		return project;
 	}
+	
+	public Iterable<Project> findAllProjects(){
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if(project == null) {
+			throw new ProjectIdException("Cannot delete project with ID '"+projectId+"'. This project does not exists.");
+		}
+		projectRepository.delete(project);
+	}
 }
