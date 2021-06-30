79f53406792c9c904e863582328aa74a0898d3a9
Nitish Kumar <nitishkmr096@gmail.com>
2021-06-30 12:00:55 +0530

    Add Redux for state mgmt

--- manageit-client/src/App.js
+++ manageit-client/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addproject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addproject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
