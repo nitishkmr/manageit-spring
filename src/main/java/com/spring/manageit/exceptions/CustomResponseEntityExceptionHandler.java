f0a66b83d2afcffd0e45c8c499df3f8b420eecf4
Nitish Kumar <nitishkmr096@gmail.com>
2021-07-09 12:25:19 +0530

    Duplicate username exception handler

--- src/main/java/com/spring/manageit/exceptions/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/spring/manageit/exceptions/CustomResponseEntityExceptionHandler.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@ControllerAdvice	
 @RestController // Since this provides a response back (ResponseEntity)
 public class CustomResponseEntityExceptionHandler {
-
+		
+	// for better error response, every throw exception will go through here through its respected ExceptionHandler
+	
 	@ExceptionHandler
 	public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request){
 		ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
@@ -20,7 +22,12 @@ public class CustomResponseEntityExceptionHandler {
 	@ExceptionHandler
 	public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request){
 		ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
-		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
-		
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);	
+	}
+	
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex, WebRequest request){
+		UserNameAlreadyExistsResponse exceptionResponse = new UserNameAlreadyExistsResponse(ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);	
 	}
 }
--- src/main/java/com/spring/manageit/services/UserService.java
+++ src/main/java/com/spring/manageit/services/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.spring.manageit.domain.User;
+import com.spring.manageit.exceptions.UsernameAlreadyExistsException;
 import com.spring.manageit.repositories.UserRepository;
 
 @Service
@@ -17,11 +18,18 @@ public class UserService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	public User saveUser(User newUser) {
-		newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 		
-		// Unique Username
-		
-		// Password and password2 should match
-		return userRepository.save(newUser);
+		try {
+			newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+			
+			// Unique Username
+			newUser.setUsername(newUser.getUsername());
+			
+			// Password and password2 should match
+			return userRepository.save(newUser);
+			
+		} catch (Exception e) {
+			throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+		}
 	}
 }
